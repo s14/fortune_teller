5f24d583e38f62077100f8c218ce216775991c9e
Raghu Betina <git@rugb.com>
2014-07-07 12:17:48 -0500

    Added root URL, fixed nav links, made lucky numbers work

--- app/controllers/fortunes_controller.rb
+++ app/controllers/fortunes_controller.rb
@@ -1,4 +1,5 @@
 class FortunesController < ApplicationController
   def numbers
+    @numbers = [rand(1..100), rand(1..100), rand(1..100), rand(1..100), rand(1..100)]
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get("/", { :controller => "fortunes", :action => "numbers" })
+
   get("/lucky_numbers", { :controller => "fortunes", :action => "numbers" })
 
   get("/zodiacs/:sign", { :controller => "zodiacs", :action => "show_details" })
