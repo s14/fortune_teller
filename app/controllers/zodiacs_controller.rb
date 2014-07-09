d15767ffb97ffd840018e20095e3c1d028068ae1
Raghu Betina <git@rugb.com>
2014-07-09 10:23:14 -0500

    Added Bootstrap and /zodiacs

--- app/controllers/zodiacs_controller.rb
+++ app/controllers/zodiacs_controller.rb
@@ -2,4 +2,8 @@ class ZodiacsController < ApplicationController
   def show_details
     @zodiac = Zodiac.find_by({ :sign => params["sign"] })
   end
+
+  def index
+    @zodiac_hashes = Zodiac.all
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   get("/lucky_numbers", { :controller => "fortunes", :action => "numbers" })
 
   get("/zodiacs/:sign", { :controller => "zodiacs", :action => "show_details" })
+
+  get("/zodiacs", { :controller => "zodiacs", :action => "index" })
 end
